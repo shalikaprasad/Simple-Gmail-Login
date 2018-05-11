bae159b6807db1c67fec4ce1ea2457c25bf6537c
Your Name <you@example.com>
2018-05-10 21:09:04 -0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Gmail_Log
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.gmail_web = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Gmail_Log.Properties.Resources.gm;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // gmail_web
+            // 
+            this.gmail_web.AllowWebBrowserDrop = false;
+            this.gmail_web.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gmail_web.IsWebBrowserContextMenuEnabled = false;
+            this.gmail_web.Location = new System.Drawing.Point(7, 66);
+            this.gmail_web.MinimumSize = new System.Drawing.Size(20, 20);
+            this.gmail_web.Name = "gmail_web";
+            this.gmail_web.Size = new System.Drawing.Size(549, 420);
+            this.gmail_web.TabIndex = 1;
+            this.gmail_web.Url = new System.Uri("https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.goog" +
+        "le.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=Serv" +
+        "iceLogin", System.UriKind.Absolute);
+            this.gmail_web.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.gmail_web_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 490);
+            this.Controls.Add(this.gmail_web);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Gmail";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.WebBrowser gmail_web;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gmail_Log
+{
+    public partial class Form1 : Form
+    {
+        public bool n = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gmail_web_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            
+                try
+                {
+                    HtmlDocument doc = gmail_web.Document;
+                    HtmlElement email = doc.GetElementById("identifierId");
+                    HtmlElement next = doc.GetElementById("identifierNext");
+                    email.SetAttribute("value", "shalikamanchanayaka38@gmail.com");
+                    next.InvokeMember("click");
+                }
+                catch
+                {
+                    
+                }
+            
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
